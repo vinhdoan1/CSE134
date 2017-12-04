ad768fac5493c35fdbcbf7f53224d8e8314c0cfd
Allen Ordookhanians <aordookh@ucsd.edu>
2017-12-04 11:48:29 -0800

    added the db instance to database.js

---
+++ public/hw5/js/database.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyC5ELIjf0XaLwgtOHKPjijx_p9Ihcg2fWA",
+    authDomain: "cse134-bfd99.firebaseapp.com",
+    databaseURL: "https://cse134-bfd99.firebaseio.com",
+    projectId: "cse134-bfd99",
+    storageBucket: "cse134-bfd99.appspot.com",
+    messagingSenderId: "793962632230"
+  };
+  firebase.initializeApp(config);
