193cc9c21b4b08ebada64e61834926c990f2feb5
Allen Ordookhanians <aordookh@ucsd.edu>
2017-12-07 11:31:48 -0800

    schedule.js is almsot done

--- public/hw5/js/schedule.js
+++ public/hw5/js/schedule.js
@@ -12,7 +12,6 @@ function parseDateAndTime(datestr, timestr){
 function validateGameForm(form, action){
   var incomplete = false;
   var game = {
-    id: "",
     opponent: "",
     location: "",
     date: "",
@@ -21,7 +20,8 @@ function validateGameForm(form, action){
     active: true
   }
   game.opponent = form.elements['gameopponent'].value;
-  incomplete = game.opponent == "Choose Opponent" || game.opponent == "";
+  //incomplete = game.opponent == "Choose Opponent" || game.opponent == "";
+  incomplete = game.opponent == "Lions";
   game.location = form.elements['gamelocation'].value;
   incomplete = incomplete || game.location == "";
   game.date = form.elements['gamedate'].value;
@@ -36,13 +36,11 @@ function validateGameForm(form, action){
   else{
     error_msg.style.display = 'none';
     if(action == "add"){
-      game.id = api.generateID();
       game.active = true;
     }
     else if(action == "edit"){
       var state = mainState.getState();
-      var gameid = state.gameID;
-      game.id = gameid;
+      var teamID = state.teamID
     }
     updateGame(game, action);
   }
@@ -50,18 +48,15 @@ function validateGameForm(form, action){
 
 function updateGame(game, action){
   var state = mainState.getState();
-  var gameID = state.gameID;
   var teamID = state.teamID;
 
-  var gamesList = api.getTeamGames(teamID);
+  var gamesList = firedatabase.getTeamGames(teamID);
   if(gamesList == null){
     gamesList = {};
   }
 
-  var exists = gamesList.some(function (other) {
-    return other.opponent == game.opponent && other.location == game.location &&
-      other.date == game.date && other.time == game.time;
-  });
+  //var exists = gamesList.gameID
+  var exists = false
   if(exists){
     var duplicate_msg = action == "add" ? document.getElementById('addgame_duplicate') : document.getElementById('editgame_duplicate');
     duplicate_msg.style.display = 'block';
@@ -69,52 +64,71 @@ function updateGame(game, action){
   else{
     var returnTo="";
     if(action == "edit"){
-      setTeamGame(teamID, gameID, game);
-      gamesList = api.getTeamGames(teamID);
+      firedatabase.updateGame(teamID, gameID, game);
+      gamesList = firedatabase.getTeamGames(teamID);
       returnTo = 'gamedetails.html';
     }
     else if(action == "add"){
-      gamesList.push(game);
+      firedatabase.addNewGame(teamID, game)
       returnTo = 'schedule.html';
-    } 
-    gamesList.sort(function(a,b){
-      return new Date(a.date) - new Date(b.date);
-    });
-    api.setTeamGames(state.teamID, gamesList);
+    }
+    // TODO: sort the gamesList
+    //gamesList.sort(function(a,b){
+      //return new Date(a.date) - new Date(b.date);
+    //});
+    //api.setTeamGames(state.teamID, gamesList);
     window.location= returnTo;
   }
 }
 
 //for debugging purposes
-function clearSchedule(){
-  var state = mainState.getState();
-  api.setTeamGames(state.teamID, new Array());
-  location.reload();
-}
+//function clearSchedule(){
+  //var state = mainState.getState();
+  //api.setTeamGames(state.teamID, new Array());
+  //location.reload();
+//}
 
 function loadSchedule(){
   var state = mainState.getState();
-  var gamesList= api.getTeamGames(state.teamID);
+  var gamesList = firedatabase.getTeamGames(state.teamID).then(function(games){
+    return games.val()
+  });
+
+
   var emptyschedule = document.getElementById('emptyschedule');
-  if(gamesList.length == 0){
+  if(gamesList.length === null){
     emptyschedule.style.display = 'block';
   }
   else{
     emptyschedule.style.display = 'none';
-    for(var i = 0; i < gamesList.length; i++){
-      var game = gamesList[i];
-      if(game.active){
-        let btn = createGameButtonDetail(game);
-        document.getElementById('schedulecontainer').appendChild(btn);
+    gamesList.then(function(game){
+
+      var keys = Object.keys(game)
+      var values = Object.values(game)
+
+      for (var i = 0; i < values.length; i++){
+
+        values[i].id = keys[i]
       }
-    }
+      values.sort(function(a,b){
+        return new Date(a.date) - new Date(b.date);
+      });
+
+      for (var i = 0 ; i < values.length; i++){
+        if(values[i].active){
+          mainState.setState('gameID', values[i].id)
+          let btn = createGameButtonDetail(values[i]);
+          document.getElementById('schedulecontainer').appendChild(btn);
+        }
+      }
+    });
   }
 }
 
 function populateOpponentSelect(selectcontainer){
   var state = mainState.getState();
   var teamID = state.teamID;
-  var opponentList = api.getOpponents(teamID);
+  var opponentList =firedatabase.getOpponents(teamID);
   for(var i = 0; i< opponentList.length; i++){
     var opp = opponentList[i];
     var opt = document.createElement("option");
@@ -131,13 +145,12 @@ function loadAddForm(){
 //preload the edit form
 function loadEditForm(){
   var state = mainState.getState();
-  var gameID = state.gameID;
+  //var gameID = state.gameID;
   var teamID = state.teamID;
-  var game = api.getTeamGame(teamID, gameID);
+  var game = firedatabase.getTeamGame(teamID, gameID);
 
   populateOpponentSelect('editgameopponent');
 
-  // var date = parseDateAndTime(game.date, game.time);
 
   var gameopponent = document.getElementById('editgameopponent');
   var gamelocation = document.getElementById('editgamelocation');
