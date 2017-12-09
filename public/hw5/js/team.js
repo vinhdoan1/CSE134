e2bcc259359cb493b51971cc12d9b9db3141dca6
Allen Ordookhanians <aordookh@ucsd.edu>
2017-12-09 01:01:03 -0800

    added the comments

--- public/hw5/js/team.js
+++ public/hw5/js/team.js
@@ -2,6 +2,8 @@
 //loads the homePage with some data
 function loadDashboard(){
   var state = mainState.getState();
+
+  //get the team to diplay on the dashboard from db
   firestoreDB.getTeam(state.teamID).then(function (teamData){
     var team = teamData.data();
     document.getElementById("h1").innerHTML = team.name;
@@ -13,6 +15,7 @@ function loadDashboard(){
   });
 }
 
+//getting the next game to be displayed
 function getUpcomingGame(){
   var state = mainState.getState();
   var gamesList = api.getTeamGames(state.teamID);
@@ -25,6 +28,7 @@ function getUpcomingGame(){
   }
 }
 
+//when gets called redirects user to game details page
 function funToGameDetails(gameID){
   return function() {
     mainState.setState("gameID", gameID);
@@ -32,12 +36,16 @@ function funToGameDetails(gameID){
   }
 }
 
+//this is used to create buttons that display the game snapshot on schedule page
 function createGameButtonDetail(game){
 
   let state = mainState.getState();
   teamID = state.teamID;
+
+  //get the opponent to be displayed on the schedule button
   let opponent = firestoreDB.getOpponent(teamID, game.opponent).then(function(opp){
 
+    //Parse the date and time and create the button with the attributes
     var date = parseDateAndTime(game.date, game.time);
     var hours = date.getHours() % 12 == 0 ? 12 : date.getHours() % 12;
     var ampm = date.getHours() >= 12 ? "PM" : "AM";
@@ -48,11 +56,14 @@ function createGameButtonDetail(game){
     btn.innerHTML = "<p class='gamebuttondetail'>" +
     date.getMonth() < 11 ? schedule.months[date.getMonth()-1] : schedule.months[date.getMonth() + 11]
     + " " + date.getDate() + ", " + date.getFullYear() + " @ " +  hours + ":" + (date.getMinutes() <10 ?'0':'') + date.getMinutes() + ampm + " - Pigs vs. " + opp.data().name + "</p>";
+
+    //append the button to the element of html
     document.getElementById('schedulecontainer').appendChild(btn);
   });
 
 }
 
+//load the add event page
 function loadAddEventPage(){
   document.getElementById('emptyeventfeed').style.fontSize="0rem";
   //show admin stuffs
@@ -62,12 +73,17 @@ function loadAddEventPage(){
   loadStats();
 }
 
+//load the stats of the game by getting from the db
 loadStats = () => {
   const state = mainState.getState()
   let teamID = state.teamID
   let gameID = state.gameID
+
+  //this is added dynamically when new stat is added
   let divSection = document.getElementById("dynamicevents");
   let datalist = document.getElementById("playernames");
+
+  //get the players from the db for the dropdown menu
   firestoreDB.getTeamPlayers(teamID).then(function(players){
 
     players.forEach(function(player){
@@ -81,10 +97,13 @@ loadStats = () => {
   });
 
   var numEvents = 0;
+
+  //get the stats from the db to be displayed as a button on the page
   firestoreDB.getStats(teamID,gameID).then(function(stats){
 
     stats.forEach(function(stat){
 
+      //create the button and append to the html element
       let btn = document.createElement("button")
       btn.setAttribute("type", "button")
       btn.setAttribute("class", "eventfeedbutton")
@@ -101,6 +120,7 @@ loadStats = () => {
   });
 }
 
+//adding stats to the db when created
 addStat = () => {
 
   let stat
@@ -111,6 +131,7 @@ addStat = () => {
   let player = document.getElementById("eventplayername").value
 
 
+  //check what type of stat it is
   if (player === "" || type === "Choose Event" || type === ""){
 
     return false
@@ -132,6 +153,7 @@ addStat = () => {
     stat = player + " made a " + type
   }
 
+  //reset the add event form
   document.getElementById('addeventform').reset();
 
   firestoreDB.setStat(teamID, gameID, stat).then(function(){
