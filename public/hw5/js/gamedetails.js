8abfed74cfcdcf50a738659a60ce3d26f8c1d42e
vinhdoan1 <vinhdoan1@gmail.com>
2017-12-09 00:42:21 -0800

    Fixed game details page

--- public/hw5/js/gamedetails.js
+++ public/hw5/js/gamedetails.js
@@ -1,34 +1,62 @@
 function populateGameDetails(){
   loadGameDetails();
-  loadGameStats();
 }
 
 function loadGameDetails(){
   var gameID = mainState.getState().gameID;
   var teamID = mainState.getState().teamID;
-  var game = getTeamGame(teamID, gameID);
-  var myTeamName = getTeamName(teamID);
-  var date = schedule.parseDateAndTime(game.date, game.time);
-  var hours = date.getHours() % 12 == 0 ? 12 : date.getHours() % 12; 
-  var ampm = date.getHours() >= 12 ? "PM" : "AM";
+  firestoreDB.getTeamGame(teamID, gameID).then(function(gameData) {
+    var game = gameData.data();
+    var date = schedule.parseDateAndTime(game.date, game.time);
+    var hours = date.getHours() % 12 == 0 ? 12 : date.getHours() % 12;
+    var ampm = date.getHours() >= 12 ? "PM" : "AM";
 
-  var gamedetails_date = document.getElementById('gamedetails_date');
-  gamedetails_date.innerHTML = schedule.months_long[date.getMonth()-1] + " " + date.getDate() + ", " + date.getFullYear();
-  var gamedetails_time = document.getElementById('gamedetails_time');
-  gamedetails_time.innerHTML = hours + ":" + (date.getMinutes() <10 ?'0':'') + date.getMinutes() + ampm;
-  var gamedetails_location = document.getElementById('gamedetails_location');
-  gamedetails_location.innerHTML = game.location;
-  var myTeam = document.getElementById('firstteam');
-  myTeam.innerHTML = myTeamName;
-  var otherTeam = document.getElementById('secondteam');
-  otherTeam.innerHTML = game.opponent;
-
-  var myTeamLogo = document.getElementById('teamimgdetailfirst');
-  var otherTeamLogo = document.getElementById('teamimgdetailsecond');
-  var myTeamLogoImg = api.getTeam(teamID).logo;
-  var otherTeamLogoImg = getOpTeamLogo(game.opponent);
-  myTeamName.src = myTeamLogoImg;
-  otherTeamLogo.src = otherTeamLogoImg;
+    var gamedetails_date = document.getElementById('gamedetails_date');
+    gamedetails_date.innerHTML = schedule.months_long[date.getMonth()-1] + " " + date.getDate() + ", " + date.getFullYear();
+    var gamedetails_time = document.getElementById('gamedetails_time');
+    gamedetails_time.innerHTML = hours + ":" + (date.getMinutes() <10 ?'0':'') + date.getMinutes() + ampm;
+    var gamedetails_location = document.getElementById('gamedetails_location');
+    gamedetails_location.innerHTML = game.location;
+    firestoreDB.getOpponent(teamID, game.opponent).then(function(opponentData) {
+      var opponent = opponentData.data();
+      var otherTeam = document.getElementById('secondteam');
+      otherTeam.innerHTML = opponent.name;
+      var otherTeamLogo = document.getElementById('teamimgdetailsecond');
+      otherTeamLogo.src = opponent.logo;
+    });
+  });
+  firestoreDB.getTeam(teamID).then(function(teamData) {
+    var team = teamData.data();
+    var myTeam = document.getElementById('firstteam');
+    myTeam.innerText = team.name;
+    var myTeamLogo = document.getElementById('teamimgdetailfirst');
+    myTeamLogo.src = team.logo;
+  });
+  firestoreDB.getStats(teamID, gameID).then(function(statsData) {
+    var goals = 0;
+    var shotsOnGoal = 0;
+    var cornerKicks = 0;
+    var penalties = 0;
+    statsData.forEach(function(doc) {
+      var stat = doc.data()['stat'];
+      if (stat.includes("goal"))
+        goals++;
+      else if (stat.includes("shotongoal"))
+        shotsOnGoal++;
+      else if (stat.includes("cornerkick"))
+        cornerKicks++;
+      else if (stat.includes("penalties"))
+        penalties++;
+    });
+    var gamesdetails_goals = document.getElementById('gamesdetails_goals');
+    gamesdetails_goals.innerHTML = goals;
+    var gamesdetails_shotsongoals = document.getElementById('gamesdetails_shotsongoals');
+    gamesdetails_shotsongoals.innerHTML = shotsOnGoal;
+    var gamesdetails_cornerkicks = document.getElementById('gamesdetails_cornerkicks');
+    gamesdetails_cornerkicks.innerHTML = cornerKicks;
+    var gamesdetails_penalties = document.getElementById('gamesdetails_penalties');
+    gamesdetails_penalties.innerHTML = penalties;
+  });
 }
 
 function loadOpponentImage(selectid, logoid){
@@ -46,10 +74,6 @@ function loadOpponentImage(selectid, logoid){
   logo.style.height = "5 rem";
 }
 
-function loadGameStats(){
-  //get game stats when thats done 
-}
-
 var deleteState = 0; // for delete game confirmation
 function deleteGame(){
   var deleteButton = document.getElementById('gamedetails_delete');
@@ -60,7 +84,7 @@ function deleteGame(){
       deleteState = 0;
       deleteButton.value = "Delete Game";
     }, 1000);
-  } 
+  }
   else {
     var state = mainState.getState();
     var teamID = state.teamID;
