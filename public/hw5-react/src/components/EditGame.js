4feedaf40265f6a9c5d585662512b8c5b4f69a71
Carmen Tin <cjtin@ucsd.edu>
2017-12-13 00:14:26 -0800

    React Edit Game

--- public/hw5-react/src/components/EditGame.js
+++ public/hw5-react/src/components/EditGame.js
@@ -19,29 +19,34 @@ class EditGame extends Component{
   constructor(props){
     super(props);
     this.state ={
-      hideLogo: true
+      hideLogo: true,
+      opponents: []
     }
+    this.loadOpponentOptions = this.loadOpponentOptions.bind(this);
   }
 
   render(){
+
+    var opponentOptions = this.loadOpponentOptions(this.state.opponents);
     return(
       <div>
         <Header history={this.props.history} backButton homeLink="/team" logout/>
         <h2 id="h2">Edit Game</h2>
-        <div id="editgameopimgcontainer" className="teamimgcontainergame" hidden={this.state.hideLogo}>
+        <div id="editgameopimgcontainer" className="teamimgcontainergame" style={{height:0}}>
           <img id="editgameopimg" className="teamimggame" src=""/>
         </div>
         <form id="editgameform" className="gameform" name="gameform">
-            <select value=" " name="gameopponent" id="editgameopponent" className="gameformfield" onChange={()=>console.log('hi')}>
-              <option value=" " disabled="disabled">Choose Opponent</option>
-            </select>
-            <p id="addgameOR">OR</p>
-            <input type="button" id="gameformaddop" className="gameformbutton" value="Add Opponent" onClick={()=>console.log('hi')}/>
-            <input type="text" name="gamelocation" id="editgamelocation" className="gameformfield" placeholder="Location"/>
-            <input type="date" name="gamedate" id="editgamedate" className="gameformfield" placeholder="Date"/>
-            <input type="time" name="gametime" id="editgametime" className="gameformfield" placeholder="Time"/>
-            <input type="button" className="gameformbutton" value="Edit Game" onClick={()=>console.log('hi')}/>
-            <div className="message" id="editgamemsg"></div>
+          <select name="gameopponent" id="editgameopponent" defaultValue="default" className="gameformfield" onChange={() => {this.loadOpponentImage('editgameopponent','editgameopimg')}}>
+            <option disabled="disabled" value="default">Choose Opponent</option>
+            {opponentOptions}
+          </select>
+          <p id="addgameOR">OR</p>
+          <input type="button" id="gameformaddop" className="gameformbutton" value="Add Opponent" onClick={()=>this.props.history.push('/addopponent')}/>
+          <input type="text" name="gamelocation" id="editgamelocation" className="gameformfield" placeholder="Location"/>
+          <input type="date" name="gamedate" id="editgamedate" className="gameformfield" placeholder="Date"/>
+          <input type="time" name="gametime" id="editgametime" className="gameformfield" placeholder="Time"/>
+          <input type="button" className="gameformbutton" value="Edit Game" onClick={()=>this.validateGameForm(this)}/>
+          <div className="message" id="editgamemsg"></div>
         </form>
       </div>
     );
@@ -58,39 +63,55 @@ class EditGame extends Component{
   }
 
   reduxLoaded(userProfile){
-    console.log(userProfile);
-    this.populateOpponentSelect(userProfile);
+    if(userProfile.teamID != ""){
+      var teamID = userProfile.teamID;
+      var opponents = [];
+      if(teamID !== ""){
+        firestoreDB.getAllOpponents(teamID).then(function(snapshot) {
+          snapshot.forEach(function(opponent) {
+              opponents.push({
+                opID: opponent.id,
+                ...opponent.data(),
+              });
+          });
+          if(opponents.length == 0){
+            this.setState({isEmpty : true});
+          }
+          else{
+            this.setState({
+              opponents: opponents,
+            });
+          }
+        }.bind(this));
+      }
+    }
+    // this.populateOpponentSelect(userProfile);
   }
 
-  populateOpponentSelect(userProfile){
-    console.log("POS:" + userProfile);
-    // var teamID = userProfile.teamID;
-    // if(teamID !== ""){
-    //   firestoreDB.getAllOpponents(teamID).then(function(snapshot) {
-    //     snapshot.forEach(function(opponent) {
-    //       var opt = document.createElement("option");
-    //       opt.value = opponent.id;
-    //       opt.text = opponent.data().name;
-    //       document.getElementById(selectcontainer).appendChild(opt);
-    //     });
-    //   });
-    // } 
+  loadOpponentOptions(opponents){
+    return opponents.map(function(opponent, i){
+      return(
+        <option key={i} value={opponent.opID}>{opponent.name}</option>
+      );
+    }.bind(this));
   }
 
-  // populateEditGameForm(userProfile){
-  //   if(userProfile.game){
-  //     var game = userProfile.game;
-      
-  //   }
-  // }
+  loadOpponentImage(selectid, logoid){
+    var op = document.getElementById(selectid);
+    // console.log(this.state.opponents);
+    var opID = op.value;
+    console.log(opID);
+    var opponent = this.state.opponents.find(function(other){
+      return opID === other.opID;
+    });
+    console.log(opponent);
+    var logo = document.getElementById(logoid);
+    logo.src = opponent.logo;
+    logo.style.height = "5rem";
+    document.getElementById("editgameopimgcontainer").style.height = "5rem";
+  }
 
-  // validateEditGameForm(){
-  //   var incomplete = false;
-
-  // }
-
-  /*
-  validateGameForm(form, action){
+  validateGameForm(form){
     //sanity check for validgame form
     var incomplete = false;
   
@@ -104,102 +125,49 @@ class EditGame extends Component{
     }
   
     //checking the form for validations
-    game.opponent = form.elements['gameopponent'].value;
+    // game.opponent = form.elements['gameopponent'].value;
+    // incomplete = game.opponent == "Choose Opponent" || game.opponent == "";
+    // game.location = form.elements['gamelocation'].value;
+    // incomplete = incomplete || game.location == "";
+    // game.date = form.elements['gamedate'].value;
+    // incomplete = incomplete ||  game.date == "";
+    // game.time = form.elements['gametime'].value;
+    // incomplete = incomplete || game.time == "";
+
+    var opID = document.getElementById('editgameopponent').value;
+    var opponent = this.state.opponents.find(function(other){
+      return opID === other.opID;
+    });
+    game.opponent = opponent.name;
+    console.log(game.opponent);
     incomplete = game.opponent == "Choose Opponent" || game.opponent == "";
-    game.location = form.elements['gamelocation'].value;
+
+    game.location = document.getElementById('editgamelocation').value;
     incomplete = incomplete || game.location == "";
-    game.date = form.elements['gamedate'].value;
+    game.date = document.getElementById('editgamedate').value;
     incomplete = incomplete ||  game.date == "";
-    game.time = form.elements['gametime'].value;
+    game.time = document.getElementById('editgametime').value;
     incomplete = incomplete || game.time == "";
   
-    var error_msg = (action == "add") ? 'addgamemsg' : 'editgamemsg';
     if(incomplete){
-      displayMessage(error_msg, "error", "Please fill out all fields");
+      helper.displayMessage('editgamemsg', "error", "Please fill out all fields");
     }
     else{
-  
-      //check for adding or editing game
-      if(action == "add"){
-        game.active = true;
-      }
-      else if(action == "edit"){
-        var state = mainState.getState();
-        var teamID = state.teamID
-      }
-      updateGame(game, action);
+      this.updateGame(game);
     }
-  }
-
-  updateGame(game);
-
   }
 
   //update the game by calling firebase
-  updateGame(game, action){
-    var state = mainState.getState();
-    var teamID = state.teamID;
-    var gameID = state.gameID
+  updateGame(game){
+    var teamID = this.props.userProfile.teamID;
+    console.log(teamID);
+    var game = this.props.userProfile.game;
+    console.log(game);
+    firestoreDB.updateGame(teamID, game.id, game).then(function(){
+      this.props.history.push('/gamedetails');
+    }.bind(this));
 
-    //check for different action add or edit a game
-    if(action == "edit"){
-
-      //call to CRUD for editing the game
-      firestoreDB.updateGame(teamID, gameID, game).then(function(){
-        window.location='gamedetails.html';
-      });
-    }
-    else if(action == "add"){
-
-      //call to CRUD for adding a new game
-      firestoreDB.addNewGame(teamID, game).then(function(){
-        window.location='schedule.html';
-
-      });
-    }
-    // updateUpcomingGame();
   }
-
-  validateGameForm(form, action){
-    //sanity check for validgame form
-    var incomplete = false;
-  
-    //game form to be addded to the database
-    var game = {
-      opponent:"",
-      location: "",
-      date: "",
-      time: "",
-      active: true
-    }
-  
-    //checking the form for validations
-    game.opponent = form.elements['gameopponent'].value;
-    incomplete = game.opponent == "Choose Opponent" || game.opponent == "";
-    game.location = form.elements['gamelocation'].value;
-    incomplete = incomplete || game.location == "";
-    game.date = form.elements['gamedate'].value;
-    incomplete = incomplete ||  game.date == "";
-    game.time = form.elements['gametime'].value;
-    incomplete = incomplete || game.time == "";
-  
-    var error_msg = (action == "add") ? 'addgamemsg' : 'editgamemsg';
-    if(incomplete){
-      displayMessage(error_msg, "error", "Please fill out all fields");
-    }
-    else{
-  
-      //check for adding or editing game
-      if(action == "add"){
-        game.active = true;
-      }
-      else if(action == "edit"){
-        var state = mainState.getState();
-        var teamID = state.teamID
-      }
-      updateGame(game, action);
-    }
-  }*/
 
 }
 
